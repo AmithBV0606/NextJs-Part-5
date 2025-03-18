873cd9b74d50eb1d2f5a79f09ec21a34e8254230
Amith B V <amithrao0606@gmail.com>
2025-03-18 12:47:49 +0530

    Studied about Context Providers .

--- app/client-route/page.tsx
+++ app/client-route/page.tsx
@@ -18,13 +18,21 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "@/components/theme-provider";
 
 export default function ClientRoutePage() {
   const settings = {
     dots: true,
   };
+
+  const theme = useTheme();
+
   return (
     <div className="image-slider-container">
+      <h1 style={{ color: theme.colors.primary, fontSize: "30px", fontWeight: 700 }}>
+        Client route page
+      </h1>
+
       <Slider {...settings}>
         <div>
           <img src="https://picsum.photos/400/200" />
